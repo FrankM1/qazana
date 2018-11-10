aaf8404d00215ce61505e0abb0f1e896a1841d0c
Franklin Gitonga <frank@radiumthemes.com>
2018-11-10 21:54:35 +0300

    grunt config

--- .grunt-config/sass.js
+++ .grunt-config/sass.js
@@ -1,14 +1,26 @@
 const sass = {
 	dist: {
 		options: {
-			sourceMap: true
+			sourceMap: true,
+			includePaths: [
+            	"assets/dev/scss"
+        	],
 		},
+		
 		files: [ {
 			expand: true,
 			cwd: 'assets/dev/scss/direction',
 			src: '*.scss',
 			dest: 'assets/css',
 			ext: '.css'
+		},
+		{
+			expand: true,
+			cwd: 'includes/extensions',
+			dest: 'includes/extensions',
+            src: ['**/*.scss'],
+			ext: '.css',
+			
 		} ]
 	}
 };
