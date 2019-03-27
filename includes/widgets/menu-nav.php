f90b216655ac61b8de348123bd193b123f992b5b
frank.gitonga <frank@radiumthemes.com>
2019-03-27 17:17:09 +0300

    menu widget

---
+++ includes/widgets/menu-nav.php
@@ -0,0 +1,145 @@
+<?php
+namespace Qazana;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Qazana menu navigation widget.
+ *
+ * Qazana widget that allows to link and menu to a specific position on the
+ * page.
+ *
+ * @since 1.0.0
+ */
+class Widget_Menu_Navigation extends Widget_Base {
+
+	/**
+	 * Get widget name.
+	 *
+	 * Retrieve menu navigation widget name.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'menu-navigation';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * Retrieve menu navigation widget title.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Menu Anchor', 'qazana' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * Retrieve menu navigation widget icon.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-navigation';
+	}
+
+	/**
+	 * Get widget keywords.
+	 *
+	 * Retrieve the list of keywords the widget belongs to.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @return array Widget keywords.
+	 */
+	public function get_keywords() {
+		return [ 'navigation', 'menu', 'link', 'url' ];
+    }
+
+	/**
+	 * Register menu navigation widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_navigation',
+			[
+				'label' => __( 'Anchor', 'qazana' ),
+			]
+		);
+
+
+		$this->add_control(
+			'navigation',
+			[
+				'label' => __( 'The ID of Menu Anchor.', 'qazana' ),
+				'type' => Controls_Manager::TEXT,
+				'placeholder' => __( 'For Example: About', 'qazana' ),
+				'description' => __( 'This ID will be the CSS ID you will have to use in your own page, Without #.', 'qazana' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'navigation_description',
+			[
+				'type' => Controls_Manager::RAW_HTML,
+				'raw' => sprintf( __( 'Note: The ID link ONLY accepts these chars: %s', 'qazana' ), '`A-Z, a-z, 0-9, _ , -`' ),
+				'content_classes' => 'qazana-panel-alert qazana-panel-alert-warning',
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render menu navigation widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+		$navigation = $this->get_settings_for_display( 'navigation' );
+
+		if ( ! empty( $navigation ) ) {
+			$this->add_render_attribute( 'inner', 'id', sanitize_html_class( $navigation ) );
+		}
+
+		$this->add_render_attribute( 'inner', 'class', 'qazana-menu-navigation' );
+		?>
+		<div <?php $this->render_attribute_string( 'inner' ); ?>></div>
+		<?php
+	}
+
+	/**
+	 * Render menu navigation widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _content_template() {
+	}
+}
