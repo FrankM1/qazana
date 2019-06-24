38fead68c221c04b54c81c9059e73a0fa6a496ab
frank.gitonga <frank.gitonga@incentro.com>
2019-06-24 13:25:11 +0300

    lighbox fix

--- assets/dev/js/frontend/utils/lightbox.js
+++ assets/dev/js/frontend/utils/lightbox.js
@@ -33,7 +33,7 @@ LightboxModule = ViewModule.extend( {
 				},
 			},
 			selectors: {
-				links: 'a, [data-qazana-lightbox]',
+				links: 'a[data-qazana-lightbox]',
 				slideshow: {
 					activeSlide: '.swiper-slide-active',
 					prevSlide: '.swiper-slide-prev',
